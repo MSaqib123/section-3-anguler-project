7a52326d126a23011055023e4e99019a211116fb
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-09-13 16:47:50 +0500

    5. Handling Form Submission

--- src/app/_2_SolutionByTeacher/user-input/user-input.component.ts
+++ src/app/_2_SolutionByTeacher/user-input/user-input.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-user-input',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  initialinvestment = "";
+  annualinvestment = "";
+  expectedreturn = "";
+  duration = "";    
 
+  submitForm()
+  {
+    console.log(this.initialinvestment);
+    console.log(this.annualinvestment);
+    console.log(this.expectedreturn);
+    console.log(this.duration);
+  }
 }
