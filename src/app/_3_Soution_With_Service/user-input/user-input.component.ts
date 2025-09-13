89001484d942731ffd3af3e4a9ab33f46c7917bb
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-09-13 19:00:48 +0500

    adding new section

---
+++ src/app/_3_Soution_With_Service/user-input/user-input.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InvestmentInput } from '../../investment-input.model';
+
+@Component({
+  selector: 'app-user-input',
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css'
+})
+export class UserInputComponent {
+  // @Output() calculate = new EventEmitter<{initialInvestment: number; duration: number; expectedReturn: number;annualInvestment: number}>();
+  @Output() calculate = new EventEmitter<InvestmentInput>();
+
+  initialinvestment = "0";
+  annualinvestment = "0";
+  expectedreturn = "5";
+  duration = "10";    
+
+  submitForm()
+  {
+    this.calculate.emit({
+       initialInvestment: +this.initialinvestment,
+       duration: +this.duration ,
+       expectedReturn: +this.expectedreturn,
+       annualInvestment: +this.annualinvestment})
+  }
+}
