7e5bcf819ed8138247c84e7083c8a0c4979c22b6
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-09-13 07:38:53 +0500

    working on task

---
+++ src/app/inv-calculator/inv-calculator.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inv-calculator',
+  imports: [],
+  templateUrl: './inv-calculator.component.html',
+  styleUrl: './inv-calculator.component.css'
+})
+export class InvCalculatorComponent {
+
+}
