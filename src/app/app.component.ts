4293e23af015d3dd688e821f25809c7f59dccd9f
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-09-13 17:53:46 +0500

    7. Calculating the Annual Investment Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
-
 // <!--===================================-->
 // <!-- ===== _1_TaskByMe ===== -->
 // <!--===================================-->
-//#region 
+//#region
 // import { Component } from '@angular/core';
 // import { RouterOutlet } from '@angular/router';
 // import { HeaderComponent } from "./_1_TaskByMe/header/header.component";
@@ -25,29 +24,49 @@
 
 //#endregion
 
-
-
 // <!--===================================-->
 // <!-- ===== _2_SolutionByTeacher ===== -->
 // <!--===================================-->
-//#region 
+//#region
 
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./_2_SolutionByTeacher/header/header.component";
-import { UserInputComponent } from "./_2_SolutionByTeacher/user-input/user-input.component";
+import { HeaderComponent } from './_2_SolutionByTeacher/header/header.component';
+import { UserInputComponent } from './_2_SolutionByTeacher/user-input/user-input.component';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, HeaderComponent, UserInputComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'section-3-anguler-project';
-  invData : any = [];
-  getdata(obj:any){
-    this.invData = obj;
+
+
+  calculateInvestmentResults( data:{initialInvestment: number; duration: number; expectedReturn: number;annualInvestment: number})
+  {
+    const {initialInvestment,duration,expectedReturn,annualInvestment} = data;
+
+    const annualData = [];
+    let investmentValue = data.initialInvestment;
+
+    for (let i = 0; i < data.duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest =
+        investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+
+    return annualData;
   }
 }
 
