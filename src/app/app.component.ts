fe61873126a36a49d489036f390c03081fc0acc7
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-09-13 16:13:54 +0500

    3. Adding a Header Component With An Image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,44 @@
+
+// <!--===================================-->
+// <!-- ===== _1_TaskByMe ===== -->
+// <!--===================================-->
 //#region 
+// import { Component } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+// import { HeaderComponent } from "./_1_TaskByMe/header/header.component";
+// import { InvCalculatorComponent } from "./_1_TaskByMe/inv-calculator/inv-calculator.component";
+// import { ResultTableComponent } from "./_1_TaskByMe/result-table/result-table.component";
+
+// @Component({
+//   selector: 'app-root',
+//   imports: [RouterOutlet, HeaderComponent, InvCalculatorComponent, ResultTableComponent],
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.css'
+// })
+// export class AppComponent {
+//   title = 'section-3-anguler-project';
+//   invData : any = [];
+//   getdata(obj:any){
+//     this.invData = obj;
+//   }
+// }
+
+//#endregion
+
+
+
+// <!--===================================-->
+// <!-- ===== _2_SolutionByTeacher ===== -->
+// <!--===================================-->
+//#region 
+
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./_1_TaskByMe/header/header.component";
-import { InvCalculatorComponent } from "./_1_TaskByMe/inv-calculator/inv-calculator.component";
-import { ResultTableComponent } from "./_1_TaskByMe/result-table/result-table.component";
+import { HeaderComponent } from "./_2_SolutionByTeacher/header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, InvCalculatorComponent, ResultTableComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
